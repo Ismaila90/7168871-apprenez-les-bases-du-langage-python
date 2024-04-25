42536f88c070897d268fa510a2c34742c82e87ad
Ismaila90 <167998448+Ismaila90@users.noreply.github.com>
2024-04-25 10:01:35 +0100

    Mon Exo main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom = "Ismaila"
+age = 34
+print(f"je m'appelle {nom}, j'ai {age} ans.")
+#modification de l'age
+age = age + 10
+print(f"je m'appelle {nom} et j'ain maintenant {age} ans.")
