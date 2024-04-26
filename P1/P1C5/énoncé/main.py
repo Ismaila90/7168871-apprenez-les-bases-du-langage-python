b27e5a5884d6bda7e3b9dddae63a82d9cec8d2be
Ismaila90 <ismailabouko90@gmail.com>
2024-04-26 21:01:01 +0100

    merci

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+nom = "Ismaila"
+age = 34
+taille = 1.74
+est_etudiant = True
+
+#affichage des contenues
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+#affichage des types
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
+
+#Fin
--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,20 @@
-# Écrivez votre code ici !
+
+#ma liste 
+fruits = ["pomme","banane","orange"]
+#ajout des éléments
+fruits.append("kiwi")
+
+#suppression de orange
+fruits.remove("orange")
+
+#modification du deuxième élément
+fruits[1] = "ananas"
+
+#afficher longueur de ma liste
+print("la liste contient ", len(fruits)," éléments")
+
+#trier la liste
+fruits.sort()
+print(fruits)
+
+#Fin
